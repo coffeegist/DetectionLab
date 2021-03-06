e02f730044e54ab049ef24ddc37992f1a1308e9c
Adam <1392689+coffeegist@users.noreply.github.com>
2021-03-05 20:33:15 -0600

    * Update win10 custom tools

--- Vagrant/scripts/custom/install-choco-win10-extras.ps1
+++ Vagrant/scripts/custom/install-choco-win10-extras.ps1
@@ -1,1 +1,1 @@
-choco install -y --limit-output --no-progress visualstudio2019community visualstudio2019-workload-manageddesktop
+choco install -y --limit-output --no-progress visualstudio2019community visualstudio2019-workload-manageddesktop git
